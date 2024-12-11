b6da508aec22b9180166a02290105ceed201453d
Muhammad Irtaza Yahya <139238585+IrtazaShk@users.noreply.github.com>
2024-12-11 20:21:16 +0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,9 +8,6 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -69,11 +66,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
