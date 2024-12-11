6b4b3112d0a4a33571dc66cca19f877e64226e24
Muhammad Irtaza Yahya <139238585+IrtazaShk@users.noreply.github.com>
2024-12-11 20:18:14 +0500

    Update login.calls.ts

--- pages/login.calls.ts
+++ pages/login.calls.ts
@@ -5,4 +5,4 @@ export async function assertElements(page: Page){
     const loginpage = new LoginPage(page);
     await expect (loginpage.CustomerLoginHeading).toBeVisible();
     await expect (loginpage.forgetPassword).toBeVisible();
-}
+} 
